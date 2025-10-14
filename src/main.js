ae66e2164bf08d002b489a62dd2c3c415f4e0c23
andr <andr3wkm123@gmail.com>
2025-10-14 05:51:02 -0500

    basic motion

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import "./style.css"
+
+import { animate, inView } from "motion"
+
+animate (
+  "header", 
+  {
+    y: [-100, 0],
+    opacity: [0, 1]
+  }, 
+  { duration: 1, delay: 2.5},
+)
+
+animate (
+  "section.new-drop",
+  {
+    y: [-100, 0],
+    opacity: [0, 1],
+  },
+  { duration: 1, delay: 2 },
+)
+
+animate("section.content", { opacity: 0 })
+inView ("section.content", (info) => {
+  animate (info.target, { opacity: 1,}, { duration: 1, delay: 1 })
+})
