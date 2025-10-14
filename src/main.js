45ae68a73369778c2869c91d00ca6bc31b8cc243
andr <andr3wkm123@gmail.com>
2025-10-14 08:14:33 -0500

    mvp

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
-import "./style.css"
-import { animate, inView, time } from "motion"
-import { 
-  Scene, 
-  PerspectiveCamera, 
-  BoxGeometry, 
-  MeshBasicMaterial, 
-  WebGLRenderer, 
-  Mesh, 
+import './style.css'
+
+import { animate, inView } from 'motion'
+import {
   AmbientLight,
+  Clock,
   DirectionalLight,
   Group,
-  Clock
-} from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
-import { NoiseShader } from './noise-shader.js';
-import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+  Mesh,
+  MeshLambertMaterial,
+  PerspectiveCamera,
+  Scene,
+  TorusKnotGeometry,
+  WebGLRenderer,
+} from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js'
+import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js'
+import { NoiseShader } from './noise-shader'
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 const guitarTag = document.querySelector("section.guitar");
 const loaderTag = document.querySelector("div.loader");
@@ -45,15 +46,15 @@ guitarTag.appendChild(renderer.domElement);
 const ambience = new AmbientLight(0x404040);
 camera.add(ambience);
 
-const keyLight = new DirectionalLight(0xffffff, 1);
+const keyLight = new DirectionalLight(0xffffff, 2);
 keyLight.position.set(-1, 1, 3);
 camera.add(keyLight);
 
-const fillLight = new DirectionalLight(0xffffff, 0.5);
+const fillLight = new DirectionalLight(0xffffff, 2.5);
 fillLight.position.set(1, 1, 3);
 camera.add(fillLight);
 
-const backLight = new DirectionalLight(0xffffff, 1);
+const backLight = new DirectionalLight(0xffffff, 5);
 backLight.position.set(-1, 3, -1);
 camera.add(backLight);
 
@@ -110,55 +111,60 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableZoom = false;
 controls.enablePan = false;
 controls.autoRotate = true;
+controls.enableDamping = true;
 controls.autoRotateSpeed = 2;
 controls.update();
 
-camera.position.z = 2;
+camera.position.z = 2.5;
 
-const composer = new EffectComposer(renderer);
+const composer = new EffectComposer(renderer)
 
-const renderPass = new RenderPass(scene, camera);
-composer.addPass(renderPass);
+const renderPass = new RenderPass(scene, camera)
+composer.addPass(renderPass)
 
-const noisePass = new ShaderPass(NoiseShader);
-noisePass.uniforms.time.value = clock.getElapsedTime();
-noisePass.uniforms.effect.value = currentEffect;
-noisePass.uniforms.aspectRatio.value = window.innerWidth / window.innerHeight;
-composer.addPass(noisePass);
+const noisePass = new ShaderPass(NoiseShader)
+noisePass.uniforms.time.value = clock.getElapsedTime()
+noisePass.uniforms.effect.value = currentEffect
+noisePass.uniforms.aspectRatio.value = window.innerWidth / window.innerHeight
+composer.addPass(noisePass)
 
-const outputPass = new OutputPass();
-composer.addPass(outputPass);
+const outputPass = new OutputPass()
+composer.addPass(outputPass)
 
 const render = () => {
-  controls.update();
+  controls.update()
 
-  scrollGroup.rotation.set(0, window.scrollY * 0.001, 0);
+  scrollGroup.rotation.set(0, window.scrollY * 0.001, 0)
+  loadGroup.rotation.z += 0.001;
+  currentEffect += (aimEffect - currentEffect) * 0.05
 
-  currentEffect += (aimEffect - currentEffect) * 0.05;
+  noisePass.uniforms.time.value = clock.getElapsedTime()
+  noisePass.uniforms.effect.value = currentEffect
 
-  noisePass.uniforms.time.value = clock.getElapsedTime();
-  noisePass.uniforms.effect.value = currentEffect;
+  requestAnimationFrame(render)
 
-  requestAnimationFrame(render);
-  composer.render();
+  composer.render()
 }
 
 const resize = () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  noisePass.uniforms.aspectRatio.value = window.innerWidth / window.innerHeight;
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+
+  noisePass.uniforms.aspectRatio.value = window.innerWidth / window.innerHeight
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 const scroll = () => {
-  clearTimeout(timeoutEffect);
-  aimEffect = 1;
+  clearTimeout(timeoutEffect)
+
+  aimEffect = 1
 
   timeoutEffect = setTimeout(() => {
-    aimEffect = 0;
-  }, 300)
+    aimEffect = 0
+  }, 100)
 }
 
-render(); 
-window.addEventListener('resize', resize);
-windows.addEventListener('scroll', scroll);
+render()
+window.addEventListener('resize', resize)
+window.addEventListener('scroll', scroll)
