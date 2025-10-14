0cb57061ed53d5301a09bb9aebaf7ac18f3c1394
andr <andr3wkm123@gmail.com>
2025-10-14 06:20:02 -0500

    motion fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./style.css"
-
 import { animate, inView } from "motion"
+import * as THREE from 'three';
 
 animate (
   "header", 
@@ -21,6 +21,13 @@ animate (
 )
 
 animate("section.content", { opacity: 0 })
-inView ("section.content", (info) => {
+inView ("section.content", (element, info) => {
   animate (info.target, { opacity: 1,}, { duration: 1, delay: 1 })
 })
+
+const scene =  new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
